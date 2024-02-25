7ab6144d26e58fd4e5148f018cf8cbe8f955639f
Manne-Ashwini <manneashwini2000@gmail.com>
2024-02-25 23:30:26 +0530

    add config files for ec2

---
+++ ec2_instance_v2/output.tf
@@ -0,0 +1,3 @@
+output "Public_ip_Address" {
+  value = aws_instance.example.public_ip
+}
---
+++ ec2_instance_v2/variables.tf
@@ -0,0 +1,8 @@
+variable "instance_value" {
+  description = "to get the instance value"
+
+}
+variable "ami_value" {
+  description="to get ami id"
+}
+
